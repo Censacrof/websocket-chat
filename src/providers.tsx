8e4aff4a8c013c41977492bd6c195ef6165c1762
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Add theme

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { theme } from "./theme";
 
 export const AllTheProviders = ({ children }: { children: ReactNode }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
