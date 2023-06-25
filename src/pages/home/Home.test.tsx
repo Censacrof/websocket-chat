2d65794bcb21546757dbaa12768aa2fc2bba24bb
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    It renders a color mode switcher

--- src/pages/home/Home.test.tsx
+++ src/pages/home/Home.test.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
 import { renderWithProviders, screen } from "../../test-utils";
-import { Home } from "./Home";
+import { Home, HomeProps } from "./Home";
 
-const Sut: FC = (props) => <Home {...props} />;
+const Sut: FC<Partial<HomeProps>> = (props) => <Home {...props} />;
 
 describe("Home", () => {
   it("renders a title", async () => {
     renderWithProviders(<Sut />);
 
     expect(screen.getByText("Home.helloWorld")).toBeInTheDocument();
+  });
+
+  it("renders a color mode switcher", async () => {
+    renderWithProviders(
+      <Sut _ColorModeSwitcher={() => <>a color mode switcher</>} />
+    );
+
+    expect(screen.getByText("a color mode switcher")).toBeInTheDocument();
   });
 });
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,23 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { ColorModeSwitcher } from "../../components/colorModeSwitcher/ColorModeSwitcher";
 
-export const Home: FC = () => {
+export interface HomeProps {
+  _ColorModeSwitcher?: typeof ColorModeSwitcher;
+}
+
+export const Home: FC<HomeProps> = ({
+  _ColorModeSwitcher = ColorModeSwitcher,
+}) => {
   const { t } = useTranslation();
 
   return (
-    <Box>
-      <Heading>{t("Home.helloWorld")}</Heading>
-    </Box>
+    <Container>
+      <Flex>
+        <Heading>{t("Home.helloWorld")}</Heading>
+        <_ColorModeSwitcher />
+      </Flex>
+    </Container>
   );
 };
