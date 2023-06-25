4c6a94c09db0966d8cac4d7896f23f07c2857cc7
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Add user-event

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,10 +1,14 @@
 import { render, RenderOptions } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ReactElement } from "react";
 import { AllTheProviders } from "./providers";
 
 export const renderWithProviders = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
-) => render(ui, { wrapper: AllTheProviders, ...options });
+) => ({
+  container: render(ui, { wrapper: AllTheProviders, ...options }),
+  user: userEvent.setup(),
+});
 
 export * from "@testing-library/react";
