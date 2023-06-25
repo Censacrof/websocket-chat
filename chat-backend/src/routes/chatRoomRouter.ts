3895da68f95a564035249f3581d5a9a5a98205bc
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 13:24:58 +0200

    It returns chatRoom list

--- chat-backend/src/routes/chatRoomRouter.ts
+++ chat-backend/src/routes/chatRoomRouter.ts
@@ -3,5 +3,10 @@ import { ChatRoom } from "../model/chatRoom";
 
 export const chatRoomRouterFactory = (chatRooms: ChatRoom[]) => {
   const router = express.Router();
+
+  router.get("/", (req, res) => {
+    res.json(chatRooms);
+  });
+
   return router;
 };
