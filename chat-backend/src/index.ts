08416ca34ce5e7ee90eda09250cf6c554f142f12
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Add express hello world

---
+++ chat-backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+ 
+const app: express.Application = express();
+
+const port: number = 3000;
+ 
+app.get('/', (_req, _res) => {
+    _res.send("Hello world");
+});
+ 
+app.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+});
