c57de1700705d9fba5009bf743ad72d439545914
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Don't use i18n provider when testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
 import { App } from "./App.tsx";
 import "./init-i18n.ts";
+import i18n from "./init-i18n.ts";
 import { AllTheProviders } from "./providers.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AllTheProviders>
-      <App />
-    </AllTheProviders>
+    <I18nextProvider i18n={i18n}>
+      <AllTheProviders>
+        <App />
+      </AllTheProviders>
+    </I18nextProvider>
   </React.StrictMode>
 );
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,13 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./init-i18n";
 import { theme } from "./theme";
 
 export const AllTheProviders = ({ children }: { children: ReactNode }) => {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </I18nextProvider>
-  );
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
