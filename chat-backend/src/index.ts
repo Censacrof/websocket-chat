d2a5a693cb1cabdf88f8c5855ee1e2afb22baa92
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Install prettier

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
- 
+import express from "express";
+
 const app: express.Application = express();
 
 const port: number = 3000;
- 
-app.get('/', (_req, _res) => {
-    _res.send("Hello world");
+
+app.get("/", (_req, _res) => {
+  _res.send("Hello world");
 });
- 
+
 app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
+  console.log(`http://localhost:${port}/`);
 });
