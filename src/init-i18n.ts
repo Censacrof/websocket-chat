40247394e274a0915dc8e4d99590d53512b4ee08
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Add i18n to providers

--- src/init-i18n.ts
+++ src/init-i18n.ts
@@ -1,5 +1,7 @@
-import i18n from "i18next";
+import _i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+
+export const i18n = _i18n.createInstance();
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,7 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { I18nextProvider } from "react-i18next";
+import { i18n } from "./init-i18n";
 import { theme } from "./theme";
 
 export const AllTheProviders = ({ children }: { children: ReactNode }) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </I18nextProvider>
+  );
 };
