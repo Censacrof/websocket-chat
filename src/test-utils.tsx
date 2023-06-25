be0d3391a53adee66987a0c59f9c59814347a50f
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-25 11:17:46 +0200

    Don't use alias

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,10 +2,9 @@ import { render, RenderOptions } from "@testing-library/react";
 import { ReactElement } from "react";
 import { AllTheProviders } from "./main";
 
-const renderWithProviders = (
+export const renderWithProviders = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from "@testing-library/react";
-export { renderWithProviders as render };
