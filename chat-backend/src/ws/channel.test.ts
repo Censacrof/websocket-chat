579c3c4bc16014ecacb9a53c6eed3419baf1b577
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-26 23:39:49 +0200

    It responds with a ping to a pong

--- chat-backend/src/ws/channel.test.ts
+++ chat-backend/src/ws/channel.test.ts
@@ -10,4 +10,14 @@ describe("WS channel", () => {
     await waitForWsEvent(ws, "open");
     ws.on("close", () => ws.close);
   });
+
+  it("responds with a pong to a ping", async () => {
+    const { channelUrl } = getTestChannel();
+
+    const ws = new WebSocket(channelUrl);
+    await waitForWsEvent(ws, "open");
+
+    ws.ping();
+    await waitForWsEvent(ws, "pong");
+  });
 });
