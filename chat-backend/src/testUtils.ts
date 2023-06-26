a330e40a45fc8b4e4eedb941744eef65d33b8ac5
Francesco Galisi <galisifrancesco@gmail.com>
2023-06-26 11:30:32 +0200

    Extract waitFor into own function

---
+++ chat-backend/src/testUtils.ts
@@ -0,0 +1,29 @@
+export const waitFor = (
+  callback: () => void | Promise<unknown>,
+  options?: {
+    timeout?: number;
+    retryInterval?: number;
+  }
+) => {
+  return new Promise((resolve, reject) => {
+    let lastError: unknown = undefined;
+    const retryMs = options?.retryInterval || 100;
+    const timeout = setTimeout(() => {
+      clearTimeout(retry);
+      reject(lastError);
+    }, options?.timeout || 5000);
+
+    const retryFn = async () => {
+      try {
+        await callback();
+        clearTimeout(timeout);
+        resolve(undefined);
+      } catch (e) {
+        retry = setTimeout(retryFn, retryMs);
+        lastError = e;
+      }
+    };
+
+    let retry = setTimeout(retryFn, retryMs);
+  });
+};
