06fda88e1b0fe0a338ea954f720ccabcbe7abbe6
Francesco Galisi <galisifrancesco@gmail.com>
2023-07-08 08:54:27 +0200

    Fix test

--- chat-backend/src/model/chatRoom.test.ts
+++ chat-backend/src/model/chatRoom.test.ts
@@ -1,7 +1,17 @@
-import { getChatRooms } from "./chatRoom";
+import { addChatRoom, getChatRooms } from "./chatRoom";
 
 describe("ChatRoom DAO", () => {
   it("allows to create a new message in the chatRoom", async () => {
     expect(getChatRooms().entries.length).toEqual(0);
+
+    addChatRoom({
+      name: "A chat room",
+      messages: [],
+    });
+
+    expect(getChatRooms().get("A chat room")).toEqual({
+      name: "A chat room",
+      messages: [],
+    });
   });
 });
